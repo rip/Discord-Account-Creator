bd1f5e741acf749d18cfc828d05dfc3e42e433bf
ytcrackers <49995327+ytcrackers@users.noreply.github.com>
2019-04-25 22:32:40 -0400

    Update discrim-http(s).py

--- discrim-http(s).py
+++ discrim-http(s).py
@@ -8,10 +8,10 @@ with open('proxies-http(s).txt') as proxies:
 		up = token_hex(4) + '@' + token_hex(4) + '.com'
 		try:
 			t = post('https://discordapp.com/api/auth/register', timeout=10, proxies=proxies, json={'consent':'true','username':u,'email':up,'password':up}).json()
-			try:print(f'\n\033[96m{u}#'+get('https://discordapp.com/api/v6/users/@me',timeout=20,proxies=proxies,headers={'Authorization':t['token']}).json()['discriminator'],up,t['token'],p,'\033[0m\n')
+			try: print(f'\n\033[96m{u}#'+get('https://discordapp.com/api/v6/users/@me',timeout=20,proxies=proxies,headers={'Authorization':t['token']}).json()['discriminator'],up,t['token'],p,'\033[0m\n')
 			except:
-				if 'token' in t: print('\n\033[96m',up,t,p,'\033[0m\n')
-				else: print(t,p)
+				if 'token' in t: print('\n\033[96m', up, t, p, '\033[0m\n')
+				else: print(t, p)
 		except: pass
 '''discrim-http(s).py
 PoC, someone add multithreading and I'll finish it.
