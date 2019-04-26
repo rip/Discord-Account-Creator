2de5c330e299f82e5c0ce3d7beccb8c92b0eec80
ytcrackers <49995327+ytcrackers@users.noreply.github.com>
2019-04-25 21:33:43 -0400

    Update discrim-http(s).py

--- discrim-http(s).py
+++ discrim-http(s).py
@@ -5,7 +5,7 @@ with open('proxies-http(s).txt') as proxies:
 	for proxy in proxies.readlines():
 		p = proxy.rstrip('\n')
 		proxies = {'http': p, 'https': p}
-		up = token_hex(3) + '@' + token_hex(4) +'.com'
+		up = token_hex(4) + '@' + token_hex(4) + '.com'
 		try:
 			t = post('https://discordapp.com/api/auth/register', timeout=10, proxies=proxies, json={'consent':'true','username':u,'email':up,'password':up}).json()
 			try:print(f'\n\033[96m{u}#'+get('https://discordapp.com/api/v6/users/@me',timeout=20,proxies=proxies,headers={'Authorization':t['token']}).json()['discriminator'],up,t['token'],p,'\033[0m\n')
