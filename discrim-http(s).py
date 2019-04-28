6b4110e821232c3eab187c7054f13836ae8e64e6
ytcrackers <49995327+ytcrackers@users.noreply.github.com>
2019-04-28 02:38:26 -0400

    Update discrim-http(s).py

--- discrim-http(s).py
+++ discrim-http(s).py
@@ -5,15 +5,11 @@ with open('proxies-http(s).txt') as proxies:
 	for proxy in proxies.readlines():
 		p = proxy.rstrip('\n')
 		proxies = {'http': p, 'https': p}
-		up = token_hex(4) + '@' + token_hex(4) + '.com'
+		up = token_hex(5) + '@gmail.com'
 		try:
 			t = post('https://discordapp.com/api/auth/register', timeout=10, proxies=proxies, json={'consent':'true','username':u,'email':up,'password':up}).json()
 			try: print(f'\n\033[96m{u}#'+get('https://discordapp.com/api/v6/users/@me',timeout=20,proxies=proxies,headers={'Authorization':t['token']}).json()['discriminator'],up,t['token'],p,'\033[0m\n')
 			except:
-				if 'token' in t: print('\n\033[96m', up, t, p, '\033[0m\n')
-				else: print(t, p)
+				if 'token' in t: print(f'\n\033[96m{u}#???? {up} {t["token"]} {p}\033[0m\n')
+				else: print(f'\u001b[38;5;90m{t} {p}\033[0m')
 		except: pass
-'''discrim-http(s).py
-PoC, someone add multithreading and I'll finish it. (bypass more captchas with fresh proxies or something like 2captcha)
-https://i.imgur.com/nSLDJfm.png
-'''
