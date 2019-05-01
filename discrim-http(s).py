d1ace1ef5136c3bcc38a0811ae12a3ab93b14234
ytcrackers <49995327+ytcrackers@users.noreply.github.com>
2019-05-01 00:24:09 -0400

    Update discrim-http(s).py

--- discrim-http(s).py
+++ discrim-http(s).py
@@ -1,15 +1,47 @@
-u = 'au'
+u = 'au'  # desired username
+
 from secrets import token_hex
 from requests import get, post
-with open('proxies-http(s).txt') as proxies:
+
+d = 'https://discordapp.com/api/v6/users/@me'
+
+with open('proxies-http(s).txt') as proxies:  # multithreading this or something would be great
 	for proxy in proxies.readlines():
 		p = proxy.rstrip('\n')
+
 		proxies = {'http': p, 'https': p}
-		up = token_hex(5) + '@gmail.com'
-		try:
-			t = post('https://discordapp.com/api/auth/register', timeout=10, proxies=proxies, json={'consent':'true','username':u,'email':up,'password':up}).json()
-			try: print(f'\n\033[96m{u}#'+get('https://discordapp.com/api/v6/users/@me',timeout=20,proxies=proxies,headers={'Authorization':t['token']}).json()['discriminator'],up,t['token'],p,'\033[0m\n')
-			except:
-				if 'token' in t: print(f'\n\033[96m{u}#???? {up} {t["token"]} {p}\033[0m\n')
-				else: print(f'\u001b[38;5;90m{t} {p}\033[0m')
+
+		up = token_hex(5) + '@gmail.com'  # email & password
+
+		try:  # register
+
+			t = post(f'{d[0:27]}auth/register', 
+					proxies=proxies, 
+					timeout=10, 
+					json={'consent': 'true', 'username': u, 'email': up, 'password': up}
+				).json()
+
+			if 'token' in t:
+
+				try:  # get discrim #
+					print(f'\n\033[96m{u}#'+get(d,
+						proxies=proxies,
+						timeout=20,
+						headers={'Authorization': t['token']}
+					).json()['discriminator'], up, t['token'], p, '\033[0m\n')
+
+				except: 
+
+					try:  # try again without proxy
+						print(f'\n\033[96m{u}#'+get(d,
+							timeout=20,
+							headers={'Authorization': t['token']}
+						).json()['discriminator'], up, t['token'], p, '\033[0m\n')  
+
+					except:  # idk, at least print token and login to manually get discrim
+						print(f'\n\033[96m{u}#???? {up} {t["token"]} {p}\033[0m\n')
+
+			else:  # reg fail
+				print(f'\u001b[38;5;90m{t} {p}\033[0m')
+
 		except: pass
