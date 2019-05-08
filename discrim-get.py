43e749f237b48a1a7b3dd13ba35324bc35a8fa1a
ytcrackers <49995327+ytcrackers@users.noreply.github.com>
2019-05-07 23:59:03 -0400

    Delete discrim-get.py

--- discrim-get.py
+++
@@ -1,7 +0,0 @@
-t='tokenhere'  # place token
-proxies = {'http': 'proxyhere', 'https': 'proxyhere'}  # insert proxy
-from requests import get
-print(get('https://discordapp.com/api/v6/users/@me',timeout=10,
-#	proxies=proxies,  # comment out if proxy iz kill
-	headers={'Authorization':t}).json()['discriminator'])  # get discrim #
-# get discrim in case fail and locked to see if its one u want to claim instead of verifying phone
